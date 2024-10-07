8c02f6f0534a6c1eaf9d924d136b13744fa23879
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-10-07 17:29:53 +0100

    extract format function to external script

--- themes/aura/assets/product.js
+++ themes/aura/assets/product.js
@@ -316,42 +316,6 @@ function getSelectedVariant(parentSection) {
   });
 }
 
-function formatCurrency(amount, currencySymbol, locale = 'en-US', usePercision = false) {
-  const shouldUsePercision = !(amount % 1 === 0) || usePercision;
-
-  try {
-    const formatter = new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: currencySymbol,
-      minimumFractionDigits: shouldUsePercision ? 2 : 0,
-    });
-    
-    return formatter.format(amount);
-  } catch (error) {
-    // Fallback formatting
-    const formatter = new Intl.NumberFormat(locale, {
-      style: 'decimal',
-      minimumFractionDigits: shouldUsePercision ? 2 : 0,
-    });
-
-    const formattedValue = formatter.format(amount);
-
-    const determineSymbolPositionFormatter = new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: 'USD',
-      currencyDisplay: 'symbol',
-    });
-
-    const parts = determineSymbolPositionFormatter.formatToParts(1);
-    const symbolIndex = parts.findIndex(part => part.type === 'currency');
-    const isSymbolOnRight = symbolIndex === 0;
-
-    return isSymbolOnRight
-      ? `${currencySymbol} ${formattedValue}`
-      : `${formattedValue} ${currencySymbol}`;
-  }
-}
-
 /**
  * Updates product details after variant change
  * @param {HTMLElement} parentSection
