62184efd69959a7651821b9058041b5de13b47f0
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-10-08 16:27:04 +0100

    reduce money code

--- themes/harmony/assets/money.js
+++ themes/harmony/assets/money.js
@@ -1,35 +1,24 @@
 function formatCurrency(amount, currencySymbol, locale = 'en-US', usePrecision = false) {
   const shouldUsePrecision = !(amount % 1 === 0) || usePrecision;
 
-  try {
-    const formatter = new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: currencySymbol,
-      minimumFractionDigits: shouldUsePrecision ? 2 : 0,
-    });
+  const formatter = new Intl.NumberFormat(locale, {
+    style: 'decimal',
+    minimumFractionDigits: shouldUsePrecision ? 2 : 0,
+  });
 
-    return formatter.format(amount);
-  } catch (error) {
-    // Fallback formatting for when the currency symbol is invalid
-    const formatter = new Intl.NumberFormat(locale, {
-      style: 'decimal',
-      minimumFractionDigits: shouldUsePrecision ? 2 : 0,
-    });
+  const formattedValue = formatter.format(amount);
 
-    const formattedValue = formatter.format(amount);
+  const determineSymbolPositionFormatter = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'symbol',
+  });
 
-    const determineSymbolPositionFormatter = new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: 'USD',
-      currencyDisplay: 'symbol',
-    });
+  const parts = determineSymbolPositionFormatter.formatToParts(1); // format with 1 USD just to determine the position of the currency symbol
+  const symbolIndex = parts.findIndex(part => part.type === 'currency');
+  const isSymbolOnLeft = symbolIndex === 0;
 
-    const parts = determineSymbolPositionFormatter.formatToParts(1);
-    const symbolIndex = parts.findIndex(part => part.type === 'currency');
-    const isSymbolOnLeft = symbolIndex === 0;
-
-    return isSymbolOnLeft
-      ? `${currencySymbol} ${formattedValue}`
-      : `${formattedValue} ${currencySymbol}`;
-  }
+  return isSymbolOnLeft
+    ? `${currencySymbol} ${formattedValue}`
+    : `${formattedValue} ${currencySymbol}`;
 }
