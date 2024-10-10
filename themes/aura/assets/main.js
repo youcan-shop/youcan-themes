5508564b24a2e6a4b73999bd3c2ec93f3644ea7b
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-10-10 15:10:59 +0100

    define important currency related vars in the global scope

--- themes/aura/assets/main.js
+++ themes/aura/assets/main.js
@@ -1,5 +1,7 @@
 /* ----- Store currency ----- */
 const currencyCode = window.Dotshop.currency;
+const customerLocale = Dotshop.customer_locale;
+const usePrecision = shouldUsePrecision();
 /* ------------------ */
 /* ----- navbar ----- */
 /* ------------------ */
@@ -300,4 +302,36 @@ function renderTextContent(htmlContent) {
 if (FORM.errors) {
   let decodedText = decodeHtmlEntities(FORM.errors);
   notify(renderTextContent(decodedText), 'error', 20000);
+}
+
+function formatCurrency(amount, currencySymbol, locale = 'en-US', usePrecision = false) {
+  const shouldUsePrecision = !(amount % 1 === 0) || usePrecision;
+
+  const formatter = new Intl.NumberFormat(locale, {
+    style: 'decimal',
+    minimumFractionDigits: shouldUsePrecision ? 2 : 0,
+  });
+
+  const formattedValue = formatter.format(amount);
+
+  const determineSymbolPositionFormatter = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'symbol',
+  });
+
+  const parts = determineSymbolPositionFormatter.formatToParts(1); // format with 1 USD just to determine the position of the currency symbol
+  const symbolIndex = parts.findIndex(part => part.type === 'currency');
+  const isSymbolOnLeft = symbolIndex === 0;
+
+  return isSymbolOnLeft
+    ? `${currencySymbol} ${formattedValue}`
+    : `${formattedValue} ${currencySymbol}`;
+}
+
+function shouldUsePrecision() {
+  const { multicurrency_settings } = Dotshop.store;
+  const { isMulticurrencyActive, usePrecision } = multicurrency_settings;
+
+  return isMulticurrencyActive && usePrecision;
 }
