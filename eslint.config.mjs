00ce5f1165307190f6522a39d933971501576a7c
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-12-05 12:26:14 +0100

    add ESLint to top level

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,11 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    files: ["themes/**/assets/*.js"],
+    rules: {
+        semi: "error",
+        "no-unused-vars": "warn",
+    }
+  }
 ];
