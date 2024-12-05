fcfa3213d464fc59a34591c106afb3a50cca2be1
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-12-05 14:03:53 +0100

    config cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,6 @@ import shopifyEslintPlugin from '@shopify/eslint-plugin';
 const config = [
   {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
-  {
-    files: ["themes/**/assets/*.js"],
-    ignores: ["**/*.config.js"],
-    rules: {
-      semi: "error",
-      "no-unused-vars": "warn",
-    },
-  },
   ...shopifyEslintPlugin.configs.esnext,
 ];
 
