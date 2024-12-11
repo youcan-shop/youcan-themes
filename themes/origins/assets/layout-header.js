a56c1ceed24d7c00af04f45ae7194b67308862a8
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-12-11 11:43:37 +0100

    handler the search bar

--- themes/origins/assets/layout-header.js
+++ themes/origins/assets/layout-header.js
@@ -1,3 +1,20 @@
-class Header extends HTMLElement {}
+class Header extends HTMLElement {
+  constructor() {
+    super();
+    this.searchBtn = this.querySelector("[data-search-action]");
+    this.searchField = this.querySelector("[data-search-field]");
+  }
+
+  connectedCallback() {
+    this.searchHandler();
+  }
+
+  searchHandler() {
+    this.searchBtn.addEventListener("click", () => {
+      const isHidden = this.searchField.dataset.hidden === "true";
+      this.searchField.dataset.hidden = !isHidden;
+    });
+  }
+}
 
 customElements.define("yc-header", Header);
