00e703a2f30a8f680ed58f8c219ca59359809c1d
Badreddine Ibril <badreddine.ibril@youcan.shop>
2025-01-16 20:56:13 +0100

    complete the variants handler logic

--- themes/origins/assets/component-variants.js
+++ themes/origins/assets/component-variants.js
@@ -12,14 +12,14 @@ class Variant extends HTMLElement {
 
   _render() {
     this.VARIANT_NAME === "dropdown" && this.dropdown();
-    this.VARIANT_NAME === "upload-image" && this.dropdown();
+    this.VARIANT_NAME === "upload-image" && this.uploadImage();
   }
 
   dropdown() {
     const trigger = this.querySelector("[data-trigger]");
-    const placeholder = trigger.querySelector("[data-placeholder]");
     const content = this.querySelector("[data-content]");
     const options = content.querySelectorAll("[data-option]");
+    const placeholder = trigger.querySelector("[data-placeholder]");
     let state = content.dataset.hidden === "true";
 
     const setPlaceholder = (option) => {
@@ -53,7 +53,53 @@ class Variant extends HTMLElement {
     );
   }
 
-  upload_image() {}
+  uploadImage() {
+    const input = this.querySelector("input[type='file']");
+    const info = this.querySelector("[data-info]");
+    const unset = info.querySelector("[data-unset]");
+
+    const toKB = (size) => `${Math.ceil(size / 1024)} KB`;
+
+    const createPreview = () => {
+      info.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="preview" data-preview>
+          <img src="${reader.result}" width="50" height="50" />
+          <div class="info">
+            <span class="name">${file.name}</span>
+            <span class="size">${toKB(file.size)}</span>
+          </div>
+        </div>`,
+      );
+    };
+
+    const setPreview = () => {
+      preview.querySelector("img").src = reader.result;
+      preview.querySelector(".name").textContent = file.name;
+      preview.querySelector(".size").textContent = toKB(file.size);
+    };
+
+    const removePreview = () => {
+      info.querySelector("[data-preview]")?.remove();
+      input.value = null;
+    };
+
+    input.addEventListener("change", () => {
+      const file = input.files?.[0];
+      if (!file) return;
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const preview = info.querySelector("[data-preview]");
+
+        !preview ? createPreview() : setPreview();
+        unset.addEventListener("click", removePreview);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  }
 }
 
 customElements.define("yc-variant", Variant);
