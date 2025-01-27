93e04ccaa3f57e6f6461b5e668cdd3415600e951
Eihab Khan <mastereihab@gmail.com>
2025-01-27 16:06:53 +0100

    chore: cleanup

--- themes/origins/assets/constants.js
+++ themes/origins/assets/constants.js
@@ -1,7 +1,3 @@
 const PUB_SUB_EVENTS = {
-  cartUpdate: "cart-update",
-  quantityUpdate: "quantity-update",
-  optionValueSelectionChange: "option-value-selection-change",
-  variantChange: "variant-change",
-  cartError: "cart-error",
+  // TODO: List possible kv events, e.g: cartUpdate: 'cart/update'
 };
