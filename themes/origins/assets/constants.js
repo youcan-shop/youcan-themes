b46192dc68975b0410d2c92502a3489578ab5082
Eihab Khan <mastereihab@gmail.com>
2025-01-28 16:01:35 +0100

    chore: Add toast globally

--- themes/origins/assets/constants.js
+++ themes/origins/assets/constants.js
@@ -1,3 +1,7 @@
 const PUB_SUB_EVENTS = {
+  cartUpdate: "cart/update",
+  cartError: "cart/error",
   // TODO: List possible kv events, e.g: cartUpdate: 'cart/update'
 };
+
+const toast = new Toast();
