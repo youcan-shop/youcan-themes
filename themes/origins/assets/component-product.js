066bccb5e93ec56672584c9f4dcb5f4cd66d8e8b
Badreddine Ibril <badreddine.ibril@youcan.shop>
2025-02-19 12:13:00 +0100

    yc-product > last variant > disable the unavailable options

--- themes/origins/assets/component-product.js
+++ themes/origins/assets/component-product.js
@@ -6,25 +6,27 @@ class Product extends HTMLElement {
   }
 
   connectedCallback() {
+    this.disableUnavailableOptions();
     this.variants.forEach((variant) =>
       variant.addEventListener("change", () => this.onVariantChanged()),
     );
   }
 
-  onVariantChanged() {
-    const selectedOptions = Object.fromEntries(
+  get selectedOptions() {
+    return Object.fromEntries(
       this.variants.flatMap((variant) =>
         [...variant.querySelectorAll("input:checked")].map((input) => [input.name, input.value]),
       ),
     );
+  }
 
+  onVariantChanged() {
     const matchedVariant = productVariants.find(
-      (variant) => JSON.stringify(variant.variations) === JSON.stringify(selectedOptions),
+      (variant) => JSON.stringify(variant.variations) === JSON.stringify(this.selectedOptions),
     );
 
-    if (matchedVariant) {
-      this.updateVariant(matchedVariant);
-    }
+    if (matchedVariant) this.updateVariant(matchedVariant);
+    this.disableUnavailableOptions();
   }
 
   updateVariant({ id, available, price, compare_at_price }) {
@@ -34,6 +36,33 @@ class Product extends HTMLElement {
     const [priceElement, compareAtPriceElement] = this.querySelectorAll("[data-product-item]");
     priceElement.textContent = formatCurrency(price);
     compareAtPriceElement.textContent = formatCurrency(compare_at_price);
+  }
+
+  disableUnavailableOptions() {
+    const lastVariant = this.variants.at(-1);
+    if (!lastVariant) return;
+
+    const inputs = lastVariant.querySelectorAll("input");
+
+    inputs.forEach((input) => {
+      const compareOptions = { ...this.selectedOptions, [input.name]: input.value };
+      const isUnavailable = productVariants.some(
+        (variant) =>
+          JSON.stringify(variant.variations) === JSON.stringify(compareOptions) &&
+          !variant.available,
+      );
+
+      input.disabled = isUnavailable;
+      if (isUnavailable) input.checked = false;
+    });
+
+    this.productForm.toggleAttribute(
+      "not-available",
+      [...inputs].every((input) => input.disabled),
+    );
+    this.productForm.querySelector("[data-buy-button]").disabled = ![...inputs].some(
+      (input) => input.checked,
+    );
   }
 }
 
