d3d78013389ff34ba6152f52518e7a5a287e0bbb
Anas <93322743+bj-anas@users.noreply.github.com>
2025-03-20 15:18:45 +0000

    TH-377: All themes > Upsell pages don't stack up when there are more than one upsell with different weights (#232)

--- themes/aura/assets/upsell.js
+++ themes/aura/assets/upsell.js
@@ -1,20 +1,9 @@
 document.getElementById("upsell-form").addEventListener("submit", async function (event) {
   event.preventDefault();
+
   const form = event.target;
   const formData = new FormData(form);
-
   const answer = event.submitter.value;
-  formData.set("answer", answer);
-
-  const yesButton = document.querySelector(".upsell-submit-yes");
-  const noButton = document.querySelector(".upsell-submit-no");
-  const buttonText = event.submitter.querySelector(".button-text");
-  const spinnerLoader = event.submitter.querySelector(".spinner");
-
-  yesButton.disabled = true;
-  noButton.disabled = true;
-  buttonText.style.display = "none";
-  spinnerLoader.style.display = "inline-block";
 
   const upsellParams = {
     upsell_id: formData.get("upsell_id"),
@@ -24,24 +13,32 @@ document.getElementById("upsell-form").addEventListener("submit", async function
   };
 
   formData.forEach((value, key) => {
-    if (key.startsWith("product_offers")) {
-      const productId = key;
-      upsellParams.product_offers[productId] = value;
+    if (!["upsell_id", "order_id"].includes(key)) {
+      upsellParams.product_offers[key] = value;
     }
   });
 
-  try {
-    const response = await youcanjs.upsell.answer(upsellParams);
-
-    if (response.error) throw new Error(response.error);
-
-    window.location.href = "/checkout/thankyou";
-  } catch (error) {
-    yesButton.disabled = false;
-    noButton.disabled = false;
-    buttonText.style.display = "inline";
-    spinnerLoader.style.display = "none";
-
-    notify(error, "error");
-  }
+  setButtonIsLoading(event);
+  await submitAnswer(upsellParams);
 });
+
+function setButtonIsLoading(event) {
+  const [yesButton, noButton] = document.querySelectorAll('[data-upsell-submit]');
+  const buttonText = event.submitter.querySelector(".button-text");
+  const spinnerLoader = event.submitter.querySelector(".spinner");
+
+  yesButton.disabled = true;
+  noButton.disabled = true;
+  buttonText.style.display = 'none';
+  spinnerLoader.style.display = 'inline-block';
+}
+
+async function submitAnswer(upsellParams) {
+  try {
+    await youcanjs.upsell.answer(upsellParams);
+
+    window.location.reload();
+  } catch (error) {
+    window.location.href = "/checkout/thankyou";
+  }
+}
