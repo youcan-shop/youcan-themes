2c359460e78b6fe868b7ac8368e4a0d1730c07c9
Badreddine Ibril <badreddine.ibril@youcan.shop>
2026-01-12 15:49:50 +0100

    feat: bella > allahu akbar

---
+++ themes/bella/assets/core.linked-fields.js
@@ -0,0 +1,122 @@
+class LinkedFields extends HTMLElement {
+  static TYPES = ["country", "region", "city"];
+
+  constructor() {
+    super();
+    this.locale = document.documentElement.lang || "en";
+    this.countryCode = null;
+    this.regionCode = null;
+
+    for (const name of LinkedFields.TYPES) {
+      this[name] = this.querySelector(`[popover]#${name}`);
+    }
+  }
+
+  connectedCallback() {
+    this._render();
+  }
+
+  _render() {
+    this.fetchOptions();
+  }
+
+  async fetchOptions() {
+    for (const type of LinkedFields.TYPES) {
+      this[type] && (await this.fetchLocationByType(type));
+    }
+  }
+
+  setUpOptions(type, options) {
+    const select = this[type];
+    select.innerHTML = "";
+
+    options.forEach((opt, index) => {
+      const isDefault = {
+        country: opt.code == this.countryCode,
+        region: index === 0,
+        city: index === 0,
+      };
+      const label = typeof opt === "string" ? opt : opt.name;
+      const value = typeof opt === "string" ? opt : opt.code;
+
+      const option = document.createElement("button");
+      option.setAttribute("ui-slot", "select-option");
+      option.setAttribute("popovertarget", type);
+      option.setAttribute("type", "button");
+
+      option.innerHTML = `
+        <label>
+          <input type="radio" name="${type}" value="${label}" ${isDefault[type] ? "checked" : ""}  />
+          ${label}
+        </label>`;
+
+      option.addEventListener("click", () => this.onChange(value, type));
+      select.appendChild(option);
+    });
+
+    select.parentElement.findSelectedElement();
+  }
+
+  async onChange(value, type) {
+    if (type === "country") this.countryCode = value;
+    if (type === "region") this.regionCode = value;
+    for (const next of {
+      country: LinkedFields.TYPES.slice(1),
+      region: LinkedFields.TYPES.slice(2),
+    }[type] || []) {
+      this[next] && (await this.fetchLocationByType(next));
+    }
+  }
+
+  async fetchLocationByType(type) {
+    const fetchMap = {
+      country: () => youcanjs.misc.getStoreMarketCountries(),
+      region: () =>
+        youcanjs.misc.getCountryRegions(this.countryCode, this.locale),
+      city: () =>
+        youcanjs.misc.getCountryCities(
+          this.countryCode,
+          this.regionCode,
+          this.locale
+        ),
+    };
+
+    this.setIsLoading(type, true);
+
+    try {
+      const response = await fetchMap[type]?.call();
+      if (!response) throw new Error(`Unknown fetch type: ${type}`);
+
+      const map = {
+        country: () => {
+          const customerCountryExists = response.countries.some(
+            (country) => country.code === CUSTOMER_COUNTRY_CODE
+          );
+          this.countryCode = customerCountryExists
+            ? CUSTOMER_COUNTRY_CODE
+            : response.countries[0].code;
+
+          this.setUpOptions(type, response.countries);
+        },
+        region: () => {
+          this.regionCode = response.states[0].code;
+          this.setUpOptions(type, response.states);
+        },
+        city: () => this.setUpOptions(type, response.cities),
+      };
+
+      map[type]?.call();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.setIsLoading(type, false);
+    }
+  }
+
+  setIsLoading(type, is_loading) {
+    this.querySelector(`[popover]#${type}`).previousElementSibling.disabled =
+      is_loading;
+  }
+}
+
+customElements.define("ui-linked-fields", LinkedFields);
