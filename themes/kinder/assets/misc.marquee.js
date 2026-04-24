f39efc4eb09d95d3c64d1970e83e59128ae1cb12
Eihab Khan <mastereihab@gmail.com>
2026-04-24 11:24:58 +0100

    chore: add animation

---
+++ themes/kinder/assets/misc.marquee.js
@@ -0,0 +1,32 @@
+if (!customElements.get("ui-marquee")) {
+  class Marquee extends HTMLElement {
+    static observedAttributes = ["playback-state"];
+
+    constructor() {
+      super();
+      this.cloud = this.querySelector('[ui-marquee="cloud"]');
+      this.track = this.querySelector('[ui-marquee="track"]');
+    }
+
+    connectedCallback() {
+      requestAnimationFrame(() => this._render());
+    }
+
+    _render() {
+      const items = Array.from(this.cloud.children);
+      const marqueeWidth = this.offsetWidth;
+      const totalItemsWidth = items.reduce((sum, el) => sum + el.offsetWidth, 0);
+      const copiesToMake = Math.ceil(marqueeWidth / totalItemsWidth);
+
+      for (let i = 0; i < copiesToMake; i++) {
+        this.cloud.appendChild(items[i].cloneNode(true));
+      }
+
+      this.track.appendChild(this.cloud.cloneNode(true));
+
+      this.setAttribute("play-state", "running");
+    }
+  }
+
+  customElements.define("ui-marquee", Marquee);
+}
