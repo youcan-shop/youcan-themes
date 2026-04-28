28fd73621f9e16f06225c9526de64682640ada67
Badreddine Ibril <badreddine.ibril@youcan.shop>
2026-04-28 09:24:59 +0100

    chore (camisa): add marque component

---
+++ themes/camisa/assets/misc.marque.js
@@ -0,0 +1,45 @@
+if (!customElements.get("ui-marquee")) {
+  class Marquee extends HTMLElement {
+    static observedAttributes = ["playback-state", "speed"];
+
+    constructor() {
+      super();
+      this.cloud = this.querySelector('[ui-marquee="cloud"]');
+      this.track = this.querySelector('[ui-marquee="track"]');
+    }
+
+    connectedCallback() {
+      this._render();
+    }
+
+    _render() {
+      const items = Array.from(this.cloud.children);
+      console.log(items);
+
+      if (items.length < 1) return;
+
+      const marqueeWidth = this.offsetWidth;
+      const totalItemsWidth = items.reduce((sum, element) => sum + element.offsetWidth, 0);
+
+      const copiesToMake = Math.floor(marqueeWidth / totalItemsWidth);
+
+      const fragment = new DocumentFragment();
+
+      for (let i = 0; i < copiesToMake; i++) {
+        items.forEach((item) => fragment.appendChild(item.cloneNode(true)));
+      }
+
+      this.cloud.appendChild(fragment);
+      this.track.appendChild(this.cloud.cloneNode(true));
+
+      const pxPerSecond = parseFloat(this.getAttribute("speed")) || 60;
+      const filledCloudWidth = totalItemsWidth * (1 + copiesToMake);
+
+      this.style.setProperty("--playback-speed", `${Math.ceil(filledCloudWidth / pxPerSecond)}s`);
+
+      this.setAttribute("play-state", "running");
+    }
+  }
+
+  customElements.define("ui-marquee", Marquee);
+}
